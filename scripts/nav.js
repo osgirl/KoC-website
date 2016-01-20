7868c90dbf89ce0947161f251531fb28bf34b8a8
setheid <setheid@gmail.com>
2016-01-19 19:53:53 -0800

    import pics, fix nav closing

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,17 +4,41 @@ nav.$main = $('#main-nav');
 nav.$inner = $('#inner-nav');
 nav.$arrowDown = $('.fa-angle-down');
 nav.$arrowUp = $('.fa-angle-up');
+nav.$hamburger = $('#hamburger');
 
 nav.dropDown = function() {
   $('#hamburger').on('click', function(event) {
     event.preventDefault();
-    // $('#links').show();
     nav.$main.slideToggle();
   });
   $('#links').on('click', function() {
     nav.$inner.slideToggle();
     nav.$arrowUp.toggle();
     nav.$arrowDown.toggle();
+  });
+};
+
+nav.menuClose = function() {
+  nav.mainClose();
+  nav.innerClose();
+};
+
+nav.mainClose = function() {
+  $('#main-nav li:not(.submenu)').on('click', function() {
+    if (nav.$hamburger.css('display') === 'flex') {
+      nav.$main.slideUp('fast');
+      nav.$inner.slideUp('fast');
+    }
+  });
+};
+
+nav.innerClose = function() {
+  $('#inner-nav').on('click', function() {
+    if (nav.$hamburger.css('display') === 'none') {
+      nav.$inner.slideUp('fast');
+      nav.$arrowUp.toggle();
+      nav.$arrowDown.toggle();
+    }
   });
 };
 
@@ -31,5 +55,6 @@ nav.resized = function() {
 
 $(function(){
   nav.dropDown();
+  nav.menuClose();
   nav.resized();
 });
